9956e3e3c0ad167fe07bc4ef8aac7a40efa069cc
Caelum119 <ataykayirdinov@gmail.com>
2025-06-17 21:00:27 +0600

    book homework 2

--- books/models.py
+++ books/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  title = models.CharField(max_length=100, verbose_name='Напишите название книги')
+  image = models.ImageField(upload_to='book/', verbose_name='Загрузите картинку')
+  description = models.TextField(verbose_name='Дайте описание книги')
+  TYPE_BOOK = (
+    ('Science', 'Science'),
+    ('Fiction', 'Fiction'),
+    ('Adventure', 'Adventure'),
+    ('Fantasy', 'Fantasy'),
+  )
+  type_book = models.CharField(max_length=100, choices=TYPE_BOOK)
+  created_at = models.DateTimeField(auto_now_add=True)
+
+
+  def __str__(self):
+    return self.title
+  
+  class Meta:
+    verbose_name = 'Book'
+    verbose_name_plural = 'Книги'
--- books/urls.py
+++ books/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.book_post, name='book_post'),
+    path('', views.book_list_view, name='book_posts'),         # /news_posts/
+    path('<int:id>/', views.book_detail_view, name='book_detail'),  # /news_posts/1/
+    path('about/', views.book_post, name='book_post'),         # optional: /news_posts/about/
 ]
